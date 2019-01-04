9083998369a70d50a2fa6f43b3c12309d5ccb973
Arpit <35460581+arpitsri3@users.noreply.github.com>
2019-01-04 17:54:53 +0530

    Update live-test.py

--- live-test.py
+++ live-test.py
@@ -6,6 +6,10 @@ import cv2
 
 counter = 0
 
+target_emotions = ["calm", "happiness", "anger"]
+model = FERModel(target_emotions, verbose=True)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 cap = cv2.VideoCapture("./test5.mp4")
 
 fps = cap.get(cv2.CAP_PROP_FPS)
@@ -18,23 +22,18 @@ ret, frame = cap.read()
 
 emotion ="" 
 
+while ret:
+    cv2.imwrite("test.jpg",frame)
+    if counter==0:
+        emotion = model.predict("test.jpg")
+    if counter==(fps-1):
+        counter = 0
+    cv2.putText(frame, emotion, (15, 15), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.imshow("Output",frame)
+    counter = counter + 1
+    if cv2.waitKey(5) == 27:
+        break
+    ret, frame = cap.read()
 
-# cap = cv2.VideoCapture("./test5.mp4")
-# ret, frame = cap.read()
-# print(frame.shape, " " ,ret)
-# while ret:
-#     cv2.imwrite("test.jpg",frame)
-#.    if count==0:
-#
-#       emotion = model.predict("test.jpg")
-#.    if count==fps-1:
-#       count=0
-#     cv2.putText(frame, emotion, (15, 15), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
-#     cv2.imshow("Output",frame)
-#.    counter=counter+1
-#     if cv2.waitKey(5) == 27:
-#         break
-#     ret, frame = cap.read()
-
-# cv2.destroyAllWindows()
-# cap.release()
+cv2.destroyAllWindows()
+cap.release()
