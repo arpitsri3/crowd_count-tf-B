2b3294fc84f1af9157b92c243d4b1c701e8a3682
Arpit <35460581+arpitsri3@users.noreply.github.com>
2019-01-04 17:43:56 +0530

    Update live-test.py

--- live-test.py
+++ live-test.py
@@ -5,3 +5,29 @@ fps = np.int32(fps)
 print("Frames Per Second:",fps,"\n")
 
 counter = 0
+
+cap = cv2.VideoCapture("./test5.mp4")
+
+ret, frame = cap.read()
+emotion ="" 
+print(frame.shape, " " ,ret)
+
+# cap = cv2.VideoCapture("./test5.mp4")
+# ret, frame = cap.read()
+# print(frame.shape, " " ,ret)
+# while ret:
+#     cv2.imwrite("test.jpg",frame)
+#.    if count==0:
+#
+#       emotion = model.predict("test.jpg")
+#.    if count==fps-1:
+#       count=0
+#     cv2.putText(frame, emotion, (15, 15), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+#     cv2.imshow("Output",frame)
+#.    counter=counter+1
+#     if cv2.waitKey(5) == 27:
+#         break
+#     ret, frame = cap.read()
+
+# cv2.destroyAllWindows()
+# cap.release()
